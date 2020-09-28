38c3916d9af0c687c513a9f0348382ae50f57fcb
Jon Finley <lilfin19@gmail.com>
2020-09-28 15:47:09 -0500

    added nodemon correctly using frrecodecamp how to deploy a react app with an express server on heroku example

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const SELECT_ALL_ITEMS_QUERY = 'SELECT * FROM item_master';
 const SELECT_ALL_PSITEMS_QUERY = 'SELECT * FROM product_structure';
 
 const SELECT_MRFG_QUERY = "SELECT im.im_item_no, im.im_desc, im.im_itemtype  FROM item_master im WHERE im.im_itemtype = 'fg'";
-const JAWSDB_URL="mysql://q3vkaci9rd4kof20:eoi2k4tb4y8weyr4@durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/s2ccpvy7askck4c4"
+const JAWSDB_URL = "mysql://q3vkaci9rd4kof20:eoi2k4tb4y8weyr4@durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/s2ccpvy7askck4c4"
 
 
 
@@ -22,31 +22,31 @@ const JAWSDB_URL="mysql://q3vkaci9rd4kof20:eoi2k4tb4y8weyr4@durvbryvdw2sjcm5.cbe
 // })
 
 const connection = mysql.createConnection({
-    host:'durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-port:3306,
-    user:'q3vkaci9rd4kof20',
+    host: 'durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'q3vkaci9rd4kof20',
     password: 'eoi2k4tb4y8weyr4',
     database: 's2ccpvy7askck4c4'
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
     console.log("connected as id " + connection.threadId);
-  });
+});
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('go to /bikes to see bikes, got to /parts to see parts')
+    res.send('go to /bikes to see bikes, got to /parts to see parts')
 });
 app.get('/parts', (req, res) => {
     connection.query(SELECT_ALL_PARTS_QUERY, (err, results) => {
-        if(err) {
+        if (err) {
             return res.send(err)
         }
-        else{
+        else {
             return res.json({
                 data: results
             })
@@ -55,10 +55,10 @@ app.get('/parts', (req, res) => {
 });
 app.get('/psitems', (req, res) => {
     connection.query(SELECT_ALL_PSITEMS_QUERY, (err, results) => {
-        if(err) {
+        if (err) {
             return res.send(err)
         }
-        else{
+        else {
             return res.json({
                 data: results
             })
@@ -66,8 +66,8 @@ app.get('/psitems', (req, res) => {
     });
 });
 app.get('/bomx', (req, res) => {
-    const {im_item_no, grossqty}= req.query;
-    const SELECT_BOMX_QUERY=`CALL BomExplode('${im_item_no}','${grossqty}')`
+    const { im_item_no, grossqty } = req.query;
+    const SELECT_BOMX_QUERY = `CALL BomExplode('${im_item_no}','${grossqty}')`
     // const {mrfgbike} = this.state;
     // // const {mrfgbike.mrfgid,mrfgbike.mrfgqty}= req.query;
     // const SELECT_BOMX_QUERY=`CALL BomExplode('${mrfgbike.mrfgid}','${mrfgbike.mrfgqty}')`;
@@ -77,12 +77,12 @@ app.get('/bomx', (req, res) => {
         console.log("query response")
         console.log(SELECT_BOMX_QUERY)
         console.log(results)
-        if(err) {
+        if (err) {
             return res.send(err)
-            
+
         }
-        else{
-            
+        else {
+
             return res.json({
                 data: results
             })
@@ -90,8 +90,8 @@ app.get('/bomx', (req, res) => {
     });
 });
 app.get('/boml', (req, res) => {
-    const {im_item_no}= req.query;
-    const SELECT_BOML_QUERY=`CALL BomList('${im_item_no}')`
+    const { im_item_no } = req.query;
+    const SELECT_BOML_QUERY = `CALL BomList('${im_item_no}')`
     // const {mrfgbike} = this.state;
     // // const {mrfgbike.mrfgid,mrfgbike.mrfgqty}= req.query;
     // const SELECT_BOMX_QUERY=`CALL BomExplode('${mrfgbike.mrfgid}','${mrfgbike.mrfgqty}')`;
@@ -101,12 +101,12 @@ app.get('/boml', (req, res) => {
         console.log("query response")
         console.log(SELECT_BOML_QUERY)
         console.log(results)
-        if(err) {
+        if (err) {
             return res.send(err)
-            
+
         }
-        else{
-            
+        else {
+
             return res.json({
                 data: results
             })
@@ -116,10 +116,10 @@ app.get('/boml', (req, res) => {
 // get all items that have fg as a descriptio using select where sql statment
 app.get('/mrfg', (req, res) => {
     connection.query(SELECT_MRFG_QUERY, (err, results) => {
-        if(err) {
+        if (err) {
             return res.send(err)
         }
-        else{
+        else {
             return res.json({
                 data: results
             })
@@ -128,28 +128,28 @@ app.get('/mrfg', (req, res) => {
 });
 app.get('/items', (req, res) => {
     connection.query(SELECT_ALL_ITEMS_QUERY, (err, results) => {
-        if(err) {
+        if (err) {
             return res.send(err)
         }
-        else{
+        else {
             return res.json({
                 data: results
             })
         }
     });
 });
-app.get('/bikes/add', (req, res)=>{
-    const {bk_name, bk_color}= req.query;
-    const INSERT_BIKES_QUERY =`INSERT INTO bikes (bk_name,bk_color) VALUES('${bk_name}','${bk_color}')`    
+app.get('/bikes/add', (req, res) => {
+    const { bk_name, bk_color } = req.query;
+    const INSERT_BIKES_QUERY = `INSERT INTO bikes (bk_name,bk_color) VALUES('${bk_name}','${bk_color}')`
     // console.log( bk_name, bk_color);
     connection.query(INSERT_BIKES_QUERY, (err, results) => {
-    if(err) {
-        return res.send(err)
-    }
-    else{
-        return res.send('successfully added product')
-    }
-});
+        if (err) {
+            return res.send(err)
+        }
+        else {
+            return res.send('successfully added product')
+        }
+    });
 });
 
 // app.get('/items/add', (req, res)=>{
@@ -164,32 +164,32 @@ app.get('/bikes/add', (req, res)=>{
 //     }
 // });
 // });
-app.get('/items/add', (req, res)=>{
-    const {im_item_no,im_desc,im_uofm,im_oh,im_itemtype}= req.query;
-    const INSERT_ITEMS_QUERY =`INSERT INTO item_master (im_item_no,im_desc,im_uofm,im_oh,im_itemtype) VALUES('${im_item_no}','${im_desc}','${im_uofm}','${im_oh}','${im_itemtype}')`    
+app.get('/items/add', (req, res) => {
+    const { im_item_no, im_desc, im_uofm, im_oh, im_itemtype } = req.query;
+    const INSERT_ITEMS_QUERY = `INSERT INTO item_master (im_item_no,im_desc,im_uofm,im_oh,im_itemtype) VALUES('${im_item_no}','${im_desc}','${im_uofm}','${im_oh}','${im_itemtype}')`
     connection.query(INSERT_ITEMS_QUERY, (err, results) => {
-    if(err) {
-        return res.send(err)
-    }
-    else{
-        return res.send('successfully added product')
-    }
-});
+        if (err) {
+            return res.send(err)
+        }
+        else {
+            return res.send('successfully added product')
+        }
+    });
 });
 
 app.get('/bikes', (req, res) => {
     connection.query(SELECT_ALL_BIKES_QUERY, (err, results) => {
-        if(err) {
+        if (err) {
             return res.send(err)
         }
-        else{
+        else {
             return res.json({
                 data: results
             })
         }
     });
 });
-app.listen(4000,() => {
+app.listen(4000, () => {
     console.log('to to /bikes for bikes server')
 });
 
