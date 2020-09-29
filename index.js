760900fcc0fabbd0550914cb5d7ca9368bec21e7
Jon Finley <lilfin19@gmail.com>
2020-09-29 15:29:02 -0500

    a

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ if(process.env.JAWSDB_URL) {
         database: 'BikeDB'
   });
 }
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}.`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 // const connection = mysql.createConnection({
 //     host:'localhost',
 //     user:'Jon',
