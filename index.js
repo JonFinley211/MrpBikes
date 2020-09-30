1876a53050edca20c4ebbe13e2c19c94834c2b2c
Jon Finley <lilfin19@gmail.com>
2020-09-30 16:25:34 -0500

    fixed starting point of app

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('*', function(_, res) {
   res.send(path.join(__dirname, './client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
-      console.log("this is and error")
+   
     }
   })
 })
