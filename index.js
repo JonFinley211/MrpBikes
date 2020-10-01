1f75c3ec6e08013ed5339518badcff2d467ca9cb
Jon Finley <lilfin19@gmail.com>
2020-10-01 17:47:18 -0500

    client2 working

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client2/build')))
     //
     app.get('*', (req, res) => {
-      res.sendFile(path.join('index.html', { root: __dirname }))
+      res.sendFile(path.join(__dirname,'client2', 'build', 'index.html'))
     })
   }
 //   app.use(express.static(path.join(__dirname, 'build')));
