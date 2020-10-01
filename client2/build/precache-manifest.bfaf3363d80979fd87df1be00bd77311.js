d1a07e4a71a89f7ce12d4e57e3214c4abfb93e62
Jon Finley <lilfin19@gmail.com>
2020-10-01 18:27:30 -0500

    git ignore allowing build file

---
+++ client2/build/precache-manifest.bfaf3363d80979fd87df1be00bd77311.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5766e0fcac077a472cb07ba3973c30b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c53d85a5c9e3c86028b9",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1aef5b134de68ea96e41",
+    "url": "/static/css/main.9b47e61b.chunk.css"
+  },
+  {
+    "revision": "c53d85a5c9e3c86028b9",
+    "url": "/static/js/2.73ce6911.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.73ce6911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aef5b134de68ea96e41",
+    "url": "/static/js/main.8f3a8daa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3fcaca9549431b151fd0bf8d0396bffd",
+    "url": "/static/media/1.3fcaca95.jpg"
+  },
+  {
+    "revision": "14b5a2601fc78915ef7c4600d917e7e8",
+    "url": "/static/media/2.14b5a260.jpg"
+  },
+  {
+    "revision": "8dfce1a3c2659bed44d6291afdfd37e9",
+    "url": "/static/media/3.8dfce1a3.jpg"
+  },
+  {
+    "revision": "dda696d84b260c015fee086cbbb91a69",
+    "url": "/static/media/4.dda696d8.jpg"
+  },
+  {
+    "revision": "17a0c5af6825e73098637f2915d97d0a",
+    "url": "/static/media/5.17a0c5af.jpg"
+  },
+  {
+    "revision": "60fd8c1636d2dd13a99f6965de293501",
+    "url": "/static/media/OpEd-AI-Factory-833655698.60fd8c16.jpg"
+  },
+  {
+    "revision": "c842bb1b3058dea0328003e46045ea4a",
+    "url": "/static/media/Tesla_auto_bots.c842bb1b.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "c64ec5c8d3ea8c3743ca51c1fa59e50e",
+    "url": "/static/media/img-5.c64ec5c8.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "285fc40e318fc91e6868b93abbb69e38",
+    "url": "/static/media/logo.285fc40e.png"
+  }
+]);
